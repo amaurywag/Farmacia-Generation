24494022ecb5df506b28b06da666bad81783a022
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-02-27 22:00:27 -0300

    Desenvolvimento do CRUD de uma Farmácia (BackEnd) utilizando Spring Boot (Java).

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,73 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "Campo obrigatório.")
+	private String nome;
+
+	@NotBlank(message = "Campo obrigatório")
+	private String descricao;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	private BigDecimal preco;
+
+	private String tipo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
