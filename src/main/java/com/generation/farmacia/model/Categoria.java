ab2cb090c9158cc81d13cd5247bd5e90b8d574ec
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-02-27 22:37:11 -0300

    Desenvolvimento do CRUD de uma Farmácia utilizando Spring Boot (Java).

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,11 +1,17 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -20,6 +26,10 @@ public class Categoria {
 
 	@NotBlank(message = "Campo obrigatório")
 	private String tipo;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> nome;
 
 	public long getId() {
 		return id;
--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -6,10 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_produto")
@@ -27,9 +29,11 @@ public class Produto {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	private BigDecimal preco;
-
-	private String tipo;
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("tipo")
+	private Categoria categoria;
+	
 	public long getId() {
 		return id;
 	}
@@ -61,13 +65,6 @@ public class Produto {
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
+	
 
 }
